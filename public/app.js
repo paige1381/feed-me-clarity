f80b62dea41e5436abbea9421b080b19e4416e57
Paige Boyer <paige1381@gmail.com>
2018-03-20 12:58:13 -0400

    added frontend for index and show pages

--- public/app.js
+++ public/app.js
@@ -13,12 +13,56 @@ app.controller('MenuController', ['$http', function($http) {
 }]);
 
 app.controller('HomeController', ['$http', function($http) {
-  this.mobileMenu = false;
+}]);
+
+app.controller('MindController', ['$http', function($http) {
+}]);
+
+app.controller('BodyController', ['$http', function($http) {
+}]);
+
+app.controller('SoulController', ['$http', function($http) {
+}]);
+
+app.controller('BlogController', ['$http', function($http) {
+}]);
+
+app.controller('RecipeController', ['$http', function($http) {
 }]);
 
 
 app.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode({ enabled: true });
+
+  $routeProvider.when('/mind', {
+    templateUrl: 'mind.html',
+    controller: 'MindController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/body', {
+    templateUrl: 'body.html',
+    controller: 'BodyController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/soul', {
+    templateUrl: 'soul.html',
+    controller: 'SoulController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/blog', {
+    templateUrl: 'blog.html',
+    controller: 'BlogController',
+    controllerAs: 'ctrl'
+  });
+
+  $routeProvider.when('/recipe', {
+    templateUrl: 'recipe.html',
+    controller: 'RecipeController',
+    controllerAs: 'ctrl'
+  });
 
   $routeProvider.when('/', {
     templateUrl: 'home.html',
